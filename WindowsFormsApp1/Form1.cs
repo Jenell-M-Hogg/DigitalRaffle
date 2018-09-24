7997a754bf8be497156b22d5040e04f0ddac3661
Jenell-M-Hogg <jenellmhogg@live.com>
2018-09-23 21:09:39 -0400

    Initial Upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LinqToExcel;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            
+            openFileDialog1.Title = "Browse CSV Files";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Multiselect = true;
+
+            openFileDialog1.DefaultExt = "csv";
+            openFileDialog1.Filter = "CSV (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String[] tri = openFileDialog1.FileNames;
+                String fileBox = "";
+                for (int i = 0; i< tri.Length; i++)
+                {
+                    if (i != 0)
+                    {
+                        fileBox += ",";
+
+                    }
+                    fileBox += tri[i];
+
+                }
+
+                textBox1.Text = fileBox;
+                label1.Text = (tri.Length).ToString() + " files selected";
+            }
+        }
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = this.textBox1.Text;
+            var book = new LinqToExcel.ExcelQueryFactory(path);
+
+            book.AddMapping<Person>(x => x.name, "Name");
+            book.AddMapping<Person>(x => x.tickets, "# of tickets");
+
+            var query = from x in book.Worksheet<Person>()
+                        select x;
+
+            int ttltickets= 0;
+            foreach(var result in query)
+            {
+                String name = result.name;
+                ttltickets+=result.tickets;
+            }
+
+            Random r = new Random();
+            int winner = r.Next(ttltickets);
+
+            ttltickets = 0;
+            foreach (var result in query)
+            {
+                String name = result.name;
+                ttltickets += result.tickets;
+                if (ttltickets >= winner)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Person
+    {
+        public string name;
+        public int tickets;
+
+        public Person(string name, int tickets)
+        {
+            this.name = name;
+            this.tickets = tickets;
+        }
+    }
+}
